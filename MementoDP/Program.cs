4ef45542fff5301f30a55e91174cd18fd8ca9227
incemehm <chelsearmani@gmail.com>
2016-04-04 17:16:22 +0300

    Geliştirme

--- MementoDP/Program.cs
+++ MementoDP/Program.cs
@@ -10,6 +10,16 @@ namespace MementoDP
     {
         static void Main(string[] args)
         {
+            WindowsStatus status = new WindowsStatus() { IsActive = true, Theme = "White" };
+            ComputerMemory memory = new ComputerMemory();
+            memory.WindowsState = status.Save();
+
+            status.IsActive = false;
+            status.Theme = "Black";
+
+            status.Restore(memory.WindowsState);
+            Console.WriteLine(status.Theme);
+
         }
     }
 }
---
+++ MementoDP/WindowsStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoDP
+{
+    public class WindowsStatus
+    {
+        public bool IsActive { get; set; }
+        public string Theme { get; set; }
+
+        public TempWindowsStatus Save()
+        {
+            return new TempWindowsStatus
+            {
+                IsActive = this.IsActive,
+                Theme = this.Theme
+            };
+        }
+        public void Restore(TempWindowsStatus status)
+        {
+            this.IsActive = status.IsActive;
+            this.Theme = status.Theme;
+        }
+    }
+}
